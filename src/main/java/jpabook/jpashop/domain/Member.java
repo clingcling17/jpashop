a6de8cd7235fb6a6f18a1f7c88dbfa8e475f7129
seungyeon_heo <seungyeon_heo@tmaxsoft.co.kr>
2023-02-22 18:23:30 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -23,6 +23,7 @@ public class Member {
     private Address address;
 
 //    @JsonIgnore entity에 presentation 계층을 위한 로직을 넣지 않는다 (양방향 의존관계)
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
